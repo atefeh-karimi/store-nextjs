1db4713473c79ec04ea5c9ce2c429973b3c420f0
atefeh-karimi <atefehkarimikia0922@gmail.com>
2023-08-20 20:13:06 +0330

    added category

---
+++ app/products/category/page.jsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+
+const categories = [
+  {
+    name: "New Arrivals",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
+  },
+  {
+    name: "Productivity",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg",
+  },
+  {
+    name: "Workspace",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg",
+  },
+  {
+    name: "Accessories",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg",
+  },
+  {
+    name: "Sale",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg",
+  },
+];
+
+export default function Category() {
+  return (
+    <div className="bg-white">
+      <div className="py-16 sm:py-24 xl:mx-auto xl:max-w-7xl xl:px-8">
+        <div className="px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8 xl:px-0">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            Shop by Category
+          </h2>
+        </div>
+
+        <div className="flow-root mt-4">
+          <div className="-my-2">
+            <div className="box-content relative py-2 overflow-x-auto h-80 xl:overflow-visible">
+              <div className="absolute flex px-4 space-x-8 sm:px-6 lg:px-8 xl:relative xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-x-0 xl:px-0">
+                {categories.map((category) => (
+                  <a
+                    key={category.name}
+                    href={category.href}
+                    className="relative flex flex-col w-56 p-6 overflow-hidden rounded-lg h-80 hover:opacity-75 xl:w-auto"
+                  >
+                    <span aria-hidden="true" className="absolute inset-0">
+                      <Image
+                        src={category.imageSrc}
+                        alt={category.name}
+                        width="0"
+                        height="0"
+                        sizes="100vh"
+                        className="object-cover object-center w-full h-full"
+                      />
+                    </span>
+                    <span
+                      aria-hidden="true"
+                      className="absolute inset-x-0 bottom-0 opacity-50 h-2/3 bg-gradient-to-t from-gray-800"
+                    />
+                    <span className="relative mt-auto text-xl font-bold text-center text-white">
+                      {category.name}
+                    </span>
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="px-4 mt-6 sm:hidden">
+          <a
+            href="#"
+            className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500"
+          >
+            Browse all categories
+            <span aria-hidden="true"> &rarr;</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
