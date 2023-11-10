ddbdab2f2444f93ac6083da978f5e48e6a384148
atefeh-karimi <atefehkarimikia0922@gmail.com>
2023-11-10 16:07:45 +0330

    add Trending section

---
+++ app/components/storeFront/Trend.jsx
@@ -0,0 +1,134 @@
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper/modules";
+import Image from "next/image";
+import Link from "next/link";
+
+const categories = [
+  {
+    name: "Basic Tees",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
+  },
+  {
+    name: "Accessories",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/mega-menu-category-03.jpg",
+  },
+  {
+    name: "Carry",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/mega-menu-category-04.jpg",
+  },
+  {
+    name: "New Arrivals",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
+  },
+  {
+    name: "Productivity",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg",
+  },
+  {
+    name: "Workspace",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg",
+  },
+  {
+    name: "Accessories",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg",
+  },
+  {
+    name: "Sale",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg",
+  },
+  {
+    name: "New Arrivals",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-01.jpg",
+  },
+  {
+    name: "Basic Tees",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/mega-menu-01-men-category-02.jpg",
+  },
+];
+function Trend() {
+  return (
+    <div className="px-4 py-24 mx-auto max-w-7xl sm:px-6 sm:py-32 lg:px-8">
+      <div className="sm:flex sm:items-baseline sm:justify-between">
+        <h2
+          id="trending-heading"
+          className="text-2xl font-bold tracking-tight text-gray-900"
+        >
+          Trending products
+        </h2>
+        <Link
+          href="#"
+          className="hidden text-sm font-semibold text-indigo-600 hover:text-indigo-500 sm:block"
+        >
+          Browse all Trends
+          <span aria-hidden="true"> &rarr;</span>
+        </Link>
+      </div>
+      <div className="box-content relative py-2 mx-auto mt-8 overflow-x-auto f h-80 xl:overflow-visible">
+        <Swiper
+          slidesPerView={5}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="absolute flex px-4 space-x-8 sm:px-6 lg:px-8 xl:relative xl:grid xl:grid-cols-5 xl:gap-x-8 xl:space-x-0 xl:px-0"
+        >
+          {categories.map((category) => (
+            <SwiperSlide key={category.name}>
+              <a
+                href={category.href}
+                className="relative flex flex-col w-56 p-6 overflow-hidden rounded-lg h-80 hover:opacity-75 xl:w-auto"
+              >
+                <span aria-hidden="true" className="absolute inset-0">
+                  <Image
+                    src={category.imageSrc}
+                    alt=""
+                    width="0"
+                    height="0"
+                    sizes="100vh"
+                    className="object-cover object-center w-full h-full"
+                  />
+                </span>
+                <span
+                  aria-hidden="true"
+                  className="absolute inset-x-0 bottom-0 opacity-50 h-2/3 bg-gradient-to-t from-gray-800"
+                />
+                <span className="relative mt-auto text-xl font-bold text-center text-white">
+                  {category.name}
+                </span>
+              </a>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default Trend;
--- app/page.js
+++ app/page.js
@@ -3,10 +3,11 @@ import Category from "./components/storeFront/Category";
 import Featured from "./components/storeFront/Featured";
 import Favorites from "./components/storeFront/Favorites";
 import CTA from "./components/storeFront/CTA";
+import Trend from "./components/storeFront/Trend";
 
 export default function Home() {
   return (
-    <div className="min-h-screen px-4 lg:px-8">
+    <div className="">
       {/* Hero section */}
       <Hero />
       <main className="">
@@ -14,6 +15,7 @@ export default function Home() {
         <Featured />
         <Favorites />
         <CTA />
+        <Trend />
       </main>
     </div>
   );
