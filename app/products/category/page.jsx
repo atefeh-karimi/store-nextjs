8e21f4e84c67ff444521dd47fa3f5a2800c9ee4b
atefeh-karimi <atefehkarimikia0922@gmail.com>
2024-01-21 13:14:26 +0330

    clean,style category

--- app/products/category/page.jsx
+++ app/products/category/page.jsx
@@ -1,41 +1,9 @@
 import Image from "next/image";
-
-const categories = [
-  {
-    name: "New Arrivals",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-01.jpg",
-  },
-  {
-    name: "Productivity",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-02.jpg",
-  },
-  {
-    name: "Workspace",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-04.jpg",
-  },
-  {
-    name: "Accessories",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-05.jpg",
-  },
-  {
-    name: "Sale",
-    href: "#",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/home-page-01-category-03.jpg",
-  },
-];
+import { categories } from "@/public/assets/data";
 
 export default function Category() {
   return (
-    <div className="bg-white">
+    <div className="lg:min-h-[800px] bg-white">
       <div className="py-16 sm:py-24 xl:mx-auto xl:max-w-7xl xl:px-8">
         <div className="px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8 xl:px-0">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
