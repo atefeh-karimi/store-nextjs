dcb8b83ce425dd7431d95c2ac3e4b865b1d54a13
atefeh-karimi <atefehkarimikia0922@gmail.com>
2024-01-26 17:12:36 +0330

    changed font

--- app/layout.js
+++ app/layout.js
@@ -2,9 +2,13 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { CartContextProvider } from "./context";
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: "400", // if single weight, otherwise you use array like [400, 500, 700],
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Online Store",
@@ -14,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <CartContextProvider>
           <Header />
           {children}
